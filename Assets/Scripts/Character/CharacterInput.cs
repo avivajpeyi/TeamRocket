977dab1c01cb78d668b725948c82536961c4dfa4
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-02-01 06:15:18 +1100

    add player two

---
+++ Assets/Scripts/Character/CharacterInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour
+{
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    private bool input = true;
+
+
+    public bool AmIReadyForInput()
+    {
+        return input;
+    }
+
+    public void PreventFurthurInput()
+    {
+        input = false;
+    }
+
+    public void SetReadyForInput()
+    {
+        input = true;
+    }
+    
+}
