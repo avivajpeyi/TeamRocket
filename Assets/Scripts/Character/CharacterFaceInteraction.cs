eab46fd08a8e1d17b69ed4b04e67cc1be307c95d
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-02-02 02:45:30 +1100

    add texture fix and colour stealing functionallity

--- Assets/Scripts/Character/CharacterFaceInteraction.cs
+++ Assets/Scripts/Character/CharacterFaceInteraction.cs
@@ -1,38 +1,83 @@
-﻿using Tile;
+﻿using System;
+using Tile;
 using UnityEngine;
 
 namespace Character
 {
     public class CharacterFaceInteraction : MonoBehaviour
     {
-        private CharacterMaster characterMaster;
-        private CharacterSoundManager characterSoundManager;
+        private CharacterMaster m_Master;
+        private CharacterSoundManager m_Sound;
         private TileMaster m_TileMaster;
         private GameManager m_GameManager;
+        private Renderer m_Renderer;
+
+        private string myTag;
         public bool hasColour = false;
+        public bool isActive = true; // not all faces activated in multiplayer mode! 
 
 
         private void Start()
         {
-            characterMaster = gameObject.transform.root.gameObject
-                .GetComponent<CharacterMaster>();
-            characterSoundManager =
-                gameObject.transform.GetComponent<CharacterSoundManager>();
+            // external finds
             m_TileMaster = FindObjectOfType<TileMaster>();
             m_GameManager = FindObjectOfType<GameManager>();
+
+            // things that belong to me
+            m_Master = transform.root.GetComponent<CharacterMaster>();
+            m_Sound = transform.root.GetComponent<CharacterSoundManager>();
+            m_Renderer = GetComponent<Renderer>();
+            myTag = transform.root.tag;
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!m_GameManager.singlePlayerGameMode &&
-                other.gameObject.CompareTag(characterMaster.myGoalTag))
+            
+            if (other.gameObject.CompareTag(m_Master.myGoalTag))
             {
-                characterSoundManager.PlayChord();
-                var tileController = other.GetComponent<TileController>();
-                tileController.SetGoal(false, characterMaster.myTag);
-                characterMaster.numPoints++;
-                m_TileMaster.AssignNewGoal(characterMaster.myTag);
+                Debug.Log("Character Collision!");
+                // yayyy! I have gotten to my target!
+                if (m_GameManager.singlePlayerGameMode)
+                {
+                    if (!hasColour)
+                    {
+                        Celebrate();
+                        StealColour(currentGoalTile: other.gameObject);
+                    }
+                }
+                else // in multiplayer mode
+                {
+                    if (isActive)
+                    {
+                        Celebrate();
+                        SetNewGoalTile(currentGoalTile: other.gameObject);
+                    }
+                }
             }
+        }
+
+        private void Celebrate()
+        {
+            m_Sound.PlayChord();
+            m_Master.numPoints++;
+        }
+
+
+        private void SetNewGoalTile(GameObject currentGoalTile)
+        {
+            
+            currentGoalTile.GetComponent<TileController>().SetGoal(
+                m_GameManager.GetGoalColor(myTag), 
+                newTileTag: myTag
+                );
+            m_TileMaster.AssignSingleGoalTile(m_Master.myTag);
+        }
+
+
+        private void StealColour(GameObject currentGoalTile)
+        {
+            m_Renderer.material = currentGoalTile.GetComponent<Renderer>().material;
+            hasColour = true;
         }
     }
 }
---
+++ Assets/Scripts/Ui/LevelClearedUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+
+public class LevelClearedUI : MonoBehaviour
+{
+    public CharacterFaceManager m_CharacterFaceManager;
+    public GameObject LevelClearedPanel;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_CharacterFaceManager.allSidesColoured)
+            LevelClearedPanel.SetActive(true);
+        
+    }
+}
