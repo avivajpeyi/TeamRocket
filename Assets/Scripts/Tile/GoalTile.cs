d6c2b7ad2d27c47373caaf1c8fb72723ff4e11ab
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-02-02 02:45:30 +1100

    add texture fix and colour stealing functionallity

--- Assets/Scripts/Tile/GoalTile.cs
+++ Assets/Scripts/Tile/GoalTile.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//*
+//  THIS IS UNUSED IN THE WORKING VERSION OF THE GAME
+//
+//*//
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Character;
