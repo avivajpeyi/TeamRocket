ceba2d42e759f4347e70fae60b7db7c9f2729364
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-02-02 13:07:32 +1100

    add scene progression

---
+++ Assets/Scripts/Ui/SceneController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public TutorialRunner TR;
+    public GameObject GameUI;
+    public GameObject StartUI;
+    public GameObject FinishUI;
+    
+    private Camera myCam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = FindObjectOfType<Camera>();
+        if (GameUI != null)
+        {
+            GameUI.GetComponent<Canvas>().worldCamera = myCam;
+        }
+        if (StartUI != null)
+        {
+            StartUI.GetComponent<Canvas>().worldCamera = myCam;
+        }
+        if (FinishUI != null)
+        {
+            FinishUI.GetComponent<Canvas>().worldCamera = myCam;
+        }
+    }
+
+
+    /// <summary>
+    /// Called via unity button
+    /// </summary>
+    public void Play()
+    {
+        StartCoroutine(StartPlay());
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("r")) { //If you press R
+            RestartScene();
+        }
+    }
+
+    IEnumerator StartPlay()
+    {
+        GameUI.SetActive(true);
+        StartUI.SetActive(false);
+        GameManager.instance.TogglePlayersActive(true);
+        yield return new WaitForSeconds(1);
+        TR.StartRunningText();
+    }
+
+    public void NextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+    public void LoadSinglePlayerScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadMultiplayerScene()
+    {
+        SceneManager.LoadScene("Multiplayer");
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Load scene called Game
+    }
+
+}
